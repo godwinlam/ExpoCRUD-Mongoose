af91d6777792c0d9402fc53c56e24eef333f0866
Flnance-player <40353627+godwinlam@users.noreply.github.com>
2024-02-22 02:19:10 +0800

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use("/api/V1/post", require("./routes/postRoutes"));
 //home
 app.get("/",(req,res)=>{
   res.status(200).send({
-    "success":"true",
+    "success":true,
     "msg":"Node Server Running"
   })
 })
